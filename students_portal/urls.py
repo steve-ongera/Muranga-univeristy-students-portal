d002896684075db82b15c07c1f7a93a0d097bd5c
steve-ongera <gadafisteve001@gmail.com>
2025-03-30 00:47:29 +0300

    made new changes

--- students_portal/urls.py
+++ students_portal/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # Add paths for your dashboards
     path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
     path('lecturer/dashboard/', views.lecturer_dashboard, name='lecturer_dashboard'),
-    path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('mainadmin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('staff/dashboard/', views.staff_dashboard, name='staff_dashboard'),
     path('finance/dashboard/', views.finance_dashboard, name='finance_dashboard'),
 ]
